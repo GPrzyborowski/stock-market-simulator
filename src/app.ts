45f27f5bffa5bcc3dc1df156e405e1f369d98687
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-05-01 00:09:38 +0200

    add get endpoint for database testing

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import pool from './db' 
 
-const PORT = 5000
+const PORT = 3000
 const app = express()
 
 app.use(cors())
 
 app.get("/health", (req, res) => {
     res.send("ok")
+})
+
+app.get("/", async (req, res) => {
+    const data = await pool.query("SELECT * FROM bank")
+    res.status(200).json(data.rows)
 })
 
 app.listen(PORT, () => {
